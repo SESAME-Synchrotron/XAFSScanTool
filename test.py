ba1b12ee88a05fec8989f25cbbb03d54a7e62de6
Mustafa-zubi <mostafa.zoubi@sesame.org.jo>
2022-11-13 22:13:39 +0300

    simple test code

---
+++ test.py
@@ -0,0 +1,7 @@
+from electronBindingEnergies import electronBindingEnergies
+
+xx= electronBindingEnergies("Cd").getEdgeEnergy("L3")
+print(xx)
+xx= electronBindingEnergies("cd").elementExist()
+print(xx)
+# print (dir(xx))
