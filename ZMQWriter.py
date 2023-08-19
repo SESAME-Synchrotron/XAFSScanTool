b58c37a6e1494cc019ec8d308413e35fd4221836
anasmoh999 <anas.mohammad@sesame.org.jo>
2023-08-19 19:59:12 +0300

    add scan topo to the writer

--- ZMQWriter.py
+++ ZMQWriter.py
@@ -64,34 +64,61 @@ class ZMQWriter (H5Writer):
 		elif self.ZMQRType == zmq.PULL: # to support Pull if needed in the future
 			self.sock.bind(self.ZMQSender)
 
-	def reciveData(self, numPointsX, numPointsY):
+	def reciveData(self, numPointsX, numPointsY, scanTopo = "seq", arrayIndexX = None, arrayIndexY=None):
 
 		self.numXPoints = numPointsX
 		self.numYPoints = numPointsY
+		self.arrayXIndex = arrayIndexX
+		self.arrayYIndex = arrayIndexY
+		self.scanTopo = scanTopo
 
 		h5file = h5py.File(GfullH5Path, 'a')  # Reopen in append mode
 
 		missedPoints = []
 		totalPoints = 0
-		for i in range(0,self.numXPoints ):
-			h5file["/exchange/xmap/data"].resize(i+1, axis=0)
-			for j in range(0,self.numYPoints):
-				totalPoints +=1
-				data = self.sock.recv_pyobj()
-				if data == "timeout":
-					h5file["/exchange/xmap/data"].resize(j+1, axis=1)
-					h5file["/exchange/xmap/data"][:, j, :] = 0
-					missedPoints.append((i, j))
-					log.error(f"missed point ({i, j})")
-					CLIMessage(f"missed point ({i, j})", "W")
-				elif data == "scanAborted":
-					CLIMessage(f"scan has been aborted", "E")
-					break;
-				else:
-					h5file["/exchange/xmap/data"].resize(j+1, axis=1)
-					h5file["/exchange/xmap/data"][:, j, :] = data
-					CLIMessage(f"Total Points: {self.numXPoints * self.numYPoints} | current index point: {i, j} | remaining points: {self.numXPoints * self.numYPoints - totalPoints}", "I")
-					log.info(f"Total Points: {self.numXPoints * self.numYPoints} | current index point: {i, j} | remaining points: {self.numXPoints * self.numYPoints - totalPoints}")
-		h5file.close()
-		CLIMessage(f"total recieved points: {(i+1) * (j+1) - len(missedPoints)} | missed points index: {'No missed points' if len(missedPoints) == 0 else missedPoints}", "I")
-		log.info(f"total recieved points: {(i+1) * (j+1) - len(missedPoints)} | missed points index: {'No missed points' if len(missedPoints) == 0 else missedPoints}")
+		if self.scanTopo.lower()[0:3] == "seq":
+			for i in range(0,self.numXPoints):
+				h5file["/exchange/xmap/data"].resize(i+1, axis=0)
+				for j in range(0,self.numYPoints):
+					totalPoints +=1
+					data = self.sock.recv_pyobj()
+					if data == "timeout":
+						h5file["/exchange/xmap/data"].resize(j+1, axis=1)
+						h5file["/exchange/xmap/data"][:, j, :] = 0
+						missedPoints.append((i, j))
+						log.error(f"missed point ({i, j})")
+						CLIMessage(f"missed point ({i, j})", "W")
+					elif data == "scanAborted":
+						CLIMessage(f"scan has been aborted", "E")
+						break;
+					else:
+						h5file["/exchange/xmap/data"].resize(j+1, axis=1)
+						h5file["/exchange/xmap/data"][:, j, :] = data
+						CLIMessage(f"Total Points: {self.numXPoints * self.numYPoints} | current index point: {i, j} | remaining points: {self.numXPoints * self.numYPoints - totalPoints}", "I")
+						log.info(f"Total Points: {self.numXPoints * self.numYPoints} | current index point: {i, j} | remaining points: {self.numXPoints * self.numYPoints - totalPoints}")
+			h5file.close()
+			CLIMessage(f"total recieved points: {(i+1) * (j+1) - len(missedPoints)} | missed points index: {'No missed points' if len(missedPoints) == 0 else missedPoints}", "I")
+			log.info(f"total recieved points: {(i+1) * (j+1) - len(missedPoints)} | missed points index: {'No missed points' if len(missedPoints) == 0 else missedPoints}")
+		else:
+			for i,k in enumerate(self.arrayXIndex):
+				h5file["/exchange/xmap/data"].resize(i+1, axis=0)
+				for j,l in enumerate(self.arrayYIndex):
+					totalPoints +=1
+					data = self.sock.recv_pyobj()
+					if data == "timeout":
+						h5file["/exchange/xmap/data"].resize(j+1, axis=1)
+						h5file["/exchange/xmap/data"][:, l, :] = 0
+						missedPoints.append((k, l))
+						log.error(f"missed point ({k, l})")
+						CLIMessage(f"missed point ({k, l})", "W")
+					elif data == "scanAborted":
+						CLIMessage(f"scan has been aborted", "E")
+						break;
+					else:
+						h5file["/exchange/xmap/data"].resize(j+1, axis=1)
+						h5file["/exchange/xmap/data"][:, l, :] = data
+						CLIMessage(f"Total Points: {self.numXPoints * self.numYPoints} | current index point: {k, l} | remaining points: {self.numXPoints * self.numYPoints - totalPoints}", "I")
+						log.info(f"Total Points: {self.numXPoints * self.numYPoints} | current index point: {k, l} | remaining points: {self.numXPoints * self.numYPoints - totalPoints}")
+			h5file.close()
+			CLIMessage(f"total recieved points: {(i+1) * (j+1) - len(missedPoints)} | missed points index: {'No missed points' if len(missedPoints) == 0 else missedPoints}", "I")
+			log.info(f"total recieved points: {(i+1) * (j+1) - len(missedPoints)} | missed points index: {'No missed points' if len(missedPoints) == 0 else missedPoints}")
