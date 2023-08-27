414b9156e67aeed1321c90fe3f052e2ee049c214
anasmoh999 <anas.mohammad@sesame.org.jo>
2023-08-27 11:12:24 +0300

    create support IOC

---
+++ IOCs/XAFS_DAQ/XAFSApp/src/O.linux-x86_64/XAFS.dbd.d
@@ -0,0 +1,106 @@
+../O.Common/XAFS.dbd: /opt/epics/base/dbd/base.dbd \
+    /opt/epics/base/dbd/menuGlobal.dbd \
+    /opt/epics/base/dbd/menuAlarmSevr.dbd \
+    /opt/epics/base/dbd/menuAlarmStat.dbd \
+    /opt/epics/base/dbd/menuFtype.dbd \
+    /opt/epics/base/dbd/menuIvoa.dbd \
+    /opt/epics/base/dbd/menuOmsl.dbd \
+    /opt/epics/base/dbd/menuPini.dbd \
+    /opt/epics/base/dbd/menuPost.dbd \
+    /opt/epics/base/dbd/menuPriority.dbd \
+    /opt/epics/base/dbd/menuYesNo.dbd \
+    /opt/epics/base/dbd/menuSimm.dbd \
+    /opt/epics/base/dbd/menuConvert.dbd \
+    /opt/epics/base/dbd/menuScan.dbd \
+    /opt/epics/base/dbd/stdRecords.dbd \
+    /opt/epics/base/dbd/aaiRecord.dbd \
+    /opt/epics/base/dbd/dbCommon.dbd \
+    /opt/epics/base/dbd/aaoRecord.dbd \
+    /opt/epics/base/dbd/aiRecord.dbd \
+    /opt/epics/base/dbd/aoRecord.dbd \
+    /opt/epics/base/dbd/aSubRecord.dbd \
+    /opt/epics/base/dbd/biRecord.dbd \
+    /opt/epics/base/dbd/boRecord.dbd \
+    /opt/epics/base/dbd/calcRecord.dbd \
+    /opt/epics/base/dbd/calcoutRecord.dbd \
+    /opt/epics/base/dbd/compressRecord.dbd \
+    /opt/epics/base/dbd/dfanoutRecord.dbd \
+    /opt/epics/base/dbd/eventRecord.dbd \
+    /opt/epics/base/dbd/fanoutRecord.dbd \
+    /opt/epics/base/dbd/histogramRecord.dbd \
+    /opt/epics/base/dbd/longinRecord.dbd \
+    /opt/epics/base/dbd/longoutRecord.dbd \
+    /opt/epics/base/dbd/lsiRecord.dbd \
+    /opt/epics/base/dbd/lsoRecord.dbd \
+    /opt/epics/base/dbd/mbbiRecord.dbd \
+    /opt/epics/base/dbd/mbbiDirectRecord.dbd \
+    /opt/epics/base/dbd/mbboRecord.dbd \
+    /opt/epics/base/dbd/mbboDirectRecord.dbd \
+    /opt/epics/base/dbd/permissiveRecord.dbd \
+    /opt/epics/base/dbd/printfRecord.dbd \
+    /opt/epics/base/dbd/selRecord.dbd \
+    /opt/epics/base/dbd/seqRecord.dbd \
+    /opt/epics/base/dbd/stateRecord.dbd \
+    /opt/epics/base/dbd/stringinRecord.dbd \
+    /opt/epics/base/dbd/stringoutRecord.dbd \
+    /opt/epics/base/dbd/subRecord.dbd \
+    /opt/epics/base/dbd/subArrayRecord.dbd \
+    /opt/epics/base/dbd/waveformRecord.dbd \
+    /opt/epics/base/dbd/filters.dbd \
+    /opt/epics/base/dbd/devSoft.dbd \
+    /opt/epics/base/dbd/asSub.dbd \
+    /opt/epics/base/dbd/dbCore.dbd
+
+/opt/epics/base/dbd/base.dbd:
+/opt/epics/base/dbd/menuGlobal.dbd:
+/opt/epics/base/dbd/menuAlarmSevr.dbd:
+/opt/epics/base/dbd/menuAlarmStat.dbd:
+/opt/epics/base/dbd/menuFtype.dbd:
+/opt/epics/base/dbd/menuIvoa.dbd:
+/opt/epics/base/dbd/menuOmsl.dbd:
+/opt/epics/base/dbd/menuPini.dbd:
+/opt/epics/base/dbd/menuPost.dbd:
+/opt/epics/base/dbd/menuPriority.dbd:
+/opt/epics/base/dbd/menuYesNo.dbd:
+/opt/epics/base/dbd/menuSimm.dbd:
+/opt/epics/base/dbd/menuConvert.dbd:
+/opt/epics/base/dbd/menuScan.dbd:
+/opt/epics/base/dbd/stdRecords.dbd:
+/opt/epics/base/dbd/aaiRecord.dbd:
+/opt/epics/base/dbd/dbCommon.dbd:
+/opt/epics/base/dbd/aaoRecord.dbd:
+/opt/epics/base/dbd/aiRecord.dbd:
+/opt/epics/base/dbd/aoRecord.dbd:
+/opt/epics/base/dbd/aSubRecord.dbd:
+/opt/epics/base/dbd/biRecord.dbd:
+/opt/epics/base/dbd/boRecord.dbd:
+/opt/epics/base/dbd/calcRecord.dbd:
+/opt/epics/base/dbd/calcoutRecord.dbd:
+/opt/epics/base/dbd/compressRecord.dbd:
+/opt/epics/base/dbd/dfanoutRecord.dbd:
+/opt/epics/base/dbd/eventRecord.dbd:
+/opt/epics/base/dbd/fanoutRecord.dbd:
+/opt/epics/base/dbd/histogramRecord.dbd:
+/opt/epics/base/dbd/longinRecord.dbd:
+/opt/epics/base/dbd/longoutRecord.dbd:
+/opt/epics/base/dbd/lsiRecord.dbd:
+/opt/epics/base/dbd/lsoRecord.dbd:
+/opt/epics/base/dbd/mbbiRecord.dbd:
+/opt/epics/base/dbd/mbbiDirectRecord.dbd:
+/opt/epics/base/dbd/mbboRecord.dbd:
+/opt/epics/base/dbd/mbboDirectRecord.dbd:
+/opt/epics/base/dbd/permissiveRecord.dbd:
+/opt/epics/base/dbd/printfRecord.dbd:
+/opt/epics/base/dbd/selRecord.dbd:
+/opt/epics/base/dbd/seqRecord.dbd:
+/opt/epics/base/dbd/stateRecord.dbd:
+/opt/epics/base/dbd/stringinRecord.dbd:
+/opt/epics/base/dbd/stringoutRecord.dbd:
+/opt/epics/base/dbd/subRecord.dbd:
+/opt/epics/base/dbd/subArrayRecord.dbd:
+/opt/epics/base/dbd/waveformRecord.dbd:
+/opt/epics/base/dbd/filters.dbd:
+/opt/epics/base/dbd/devSoft.dbd:
+/opt/epics/base/dbd/asSub.dbd:
+/opt/epics/base/dbd/dbCore.dbd:
+../O.Common/XAFS.dbd: ../Makefile
